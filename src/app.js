7dc3058c87e6ef402e5253395dfde69e3abac074
spendrak <sonya7usekova@gmal.com>
2025-02-07 14:46:38 +0500

    Создание контекстного меню

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
 import './styles.css'
+import { ContextMenu } from './menu';
 
+const contextMenu = new ContextMenu('#menu');
+
+// здесь добавляем элементы в меню с помощью contextMenu.add
+// этот элемент тут для примера
+contextMenu.add({ text: 'first action', action: null });
