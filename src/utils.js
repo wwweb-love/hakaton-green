0b8f13753598fbea4ea3822d0fc254eb3200bd3d
Konstantin Teterin <teterinkonst@gmail.com>
2025-02-07 20:20:52 +0300

    add timer function in utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
 export function random(min, max) {
-  return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+}
+
+export function timer(callback, seconds) {
+  let timer = setInterval(() => {
+    if (seconds > 0) {
+      seconds--;
+      callback(seconds);
+      console.log(seconds);
+    } else {
+      clearInterval(timer);
+    }
+  }, 1000);
 }
