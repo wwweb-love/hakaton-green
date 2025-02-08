52fa5115cb3749e99650cc65380d722bb131e39c
Konstantin Teterin <teterinkonst@gmail.com>
2025-02-08 13:29:09 +0300

    добавил созадние нескольких таймеров с запросом кол-ва секунд с модалки

--- src/utils.js
+++ src/utils.js
@@ -2,13 +2,13 @@ export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 }
 
-export function timer(callback, seconds) {
+export function timer(callback, seconds, interval = 1000) {
   let timer = setInterval(() => {
     if (seconds > 0) {
-      seconds--;
-      callback(seconds);
+      seconds -= interval / 1000;
+      callback(Math.floor(seconds));
     } else {
       clearInterval(timer);
     }
-  }, 1000);
+  }, interval);
 }
