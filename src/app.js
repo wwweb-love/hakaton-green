9de96e0ab4ef655551c7a8ee2f66d072c6278395
Roman <rroman2115@yandex.ru>
2025-02-08 15:43:42 +0400

    edit app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import { TimerModule } from './modules/timer.module';
 
 
 // здесь добавляем элементы в меню с помощью contextMenu.add(*экземпляр класса*)
-=======
 const contextMenu = new ContextMenu('#menu');
 const timer = new TimerModule();
 const shape = new ShapeModule('shape', 'shape')
