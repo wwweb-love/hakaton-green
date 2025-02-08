876cd0b6afd16f034bf7afdd27e0c9f015acaa4c
Вероника Мухаметзянова <veronikamuhametzyanova@MacBook-Pro-Veronika.local>
2025-02-08 13:04:57 +0300

    shape-module

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import './styles.css'
 import { ContextMenu } from './menu';
+import { ShapeModule } from '../src/modules/shape.module'
 
 const contextMenu = new ContextMenu('#menu');
 
+const shape = new ShapeModule('shape', 'shape')
+contextMenu.add(shape)
 // здесь добавляем элементы в меню с помощью contextMenu.add(*экземпляр класса*)
