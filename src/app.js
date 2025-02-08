9c236c1b3365ff14ae4ed1752e84e32ca76d118b
spendrak <sonya7usekova@gmal.com>
2025-02-08 12:32:02 +0500

    реализация рандомных звуков

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import './styles.css'
 import { ContextMenu } from './menu';
+import { AudioModule } from './modules/audio.module';
 
 const contextMenu = new ContextMenu('#menu');
 
-// здесь добавляем элементы в меню с помощью contextMenu.add
-// этот элемент тут для примера
-contextMenu.add({ text: 'first action', action: null });
+// здесь добавляем элементы в меню с помощью contextMenu.add(*экземпляр класса*)
+contextMenu.add(new AudioModule());
---
+++ src/sounds/audio.source.js
@@ -0,0 +1,13 @@
+export const audioSources = [
+    './src/sounds/demotivator.mp3',
+    './src/sounds/dengi.mp3',
+    './src/sounds/gta.mp3',
+    './src/sounds/horosho.mp3',
+    './src/sounds/kurva.mp3',
+    './src/sounds/dengi.mp3',
+    './src/sounds/omg.mp3',
+    './src/sounds/povezlo.mp3',
+    './src/sounds/puk.mp3',
+    './src/sounds/sueta.mp3',
+    './src/sounds/truth.mp3',
+];
