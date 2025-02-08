b188cba48e02d9c7b8ef51814c198cef5192d6e4
Roman <rroman2115@yandex.ru>
2025-02-08 22:41:08 +0400

    second commit add func analytics click

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,8 +1,34 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
+import { ModalClick } from './modalClick.module'
+import { ModalInfoClick } from './modalClick.module'
+
+const modalClick = new ModalClick
+const modalInfoClick = new ModalInfoClick
 
 export class ClicksModule extends Module {
+
     trigger() {
-        console.log(this.type)
-        console.log(this.text)
+        this.seconds = 3
+        this.time = this.seconds
+
+        // Открываем модальное окно
+        modalClick.open()
+
+        // Запускаем отсчет времени
+        modalClick.editTime(this.seconds)
+        modalClick.countClicks = 0
+
+        let timer = setInterval(() => {
+            if (this.seconds > 1) {
+                modalClick.editTime(this.seconds - 1)
+                this.seconds -= 1
+            } else {
+                modalClick.editTime(0)
+                modalClick.editClick(0)
+                modalClick.close()
+                clearInterval(timer);
+                modalInfoClick.open(modalClick.countClicks, this.time)
+            }
+        }, 1000)
     }
 }
