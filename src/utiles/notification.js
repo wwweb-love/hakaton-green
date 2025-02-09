91a97f8f482ea683de72497b535dfccb9f0b30c9
Roman <rroman2115@yandex.ru>
2025-02-09 12:33:32 +0400

    edit analytics click

--- src/utiles/notification.js
+++ src/utiles/notification.js
@@ -1,9 +1,7 @@
 export default function AddDomNotification() {
 
-    const $body = document.querySelector('body')
-
     const $notificationContainer = document.createElement('div')
     $notificationContainer.classList.add('notification-container')
 
-    $body.append($notificationContainer)
+    document.body.append($notificationContainer)
 }
